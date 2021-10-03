fe7b2a9006612faee4afb8d4051d1ecdaff72d19
ojoksimovic <olivera2391@gmail.com>
2021-10-03 15:01:07 -0400

    add controller, update service and repo classes

--- employeemanager/src/main/java/tech/olivera/employeemanager/repo/EmployeeRepo.java
+++ employeemanager/src/main/java/tech/olivera/employeemanager/repo/EmployeeRepo.java
@@ -1,8 +1,16 @@
 package tech.olivera.employeemanager.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.olivera.employeemanager.model.Employee;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long>{
-    
+    // query methods
+    // naming convention -> delete from model (Employee) by field (id)
+
+    void deleteEmployeeById(Long id);
+
+    Optional <Employee> findEmployeeById(Long id) 
+
 }
