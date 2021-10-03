1a9eef834731542cbcde73750d90e62e75ac8bb9
ojoksimovic <olivera2391@gmail.com>
2021-10-03 15:01:07 -0400

    add controller, update service and repo classes

--- employeemanager/src/main/java/tech/olivera/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/tech/olivera/employeemanager/service/EmployeeService.java
@@ -1,10 +1,14 @@
 package tech.olivera.employeemanager.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tech.olivera.employeemanager.exception.UserNotFoundException;
 import tech.olivera.employeemanager.model.Employee;
 import tech.olivera.employeemanager.repo.EmployeeRepo;
+
 
 @Service
 public class EmployeeService {
@@ -18,5 +22,23 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeebyId(Long id){
+        return employeeRepo.findEmployeeById(id)
+        .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+ 
+    // query method
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
