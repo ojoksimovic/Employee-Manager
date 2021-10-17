7fd1c85181742b0f5df3ee8d6bd0f315bd02132c
ojoksimovic <olivera2391@gmail.com>
2021-10-17 13:59:32 -0400

    update functionality for add, edit employee

--- employeemanagerapp/src/app/app.module.ts
+++ employeemanagerapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
